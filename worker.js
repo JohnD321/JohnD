17ee705cb62bb2be07fa8842aefbf99e6e42b4f8
John D <1656101+JohnD321@users.noreply.github.com>
2024-01-27 08:24:03 -0500

    Update worker.js

--- worker.js
+++ worker.js
@@ -2,10 +2,10 @@ onmessage = function(e) {
   console.log('Worker: Message received from main script');
   const result = e.data[0] * e.data[1];
   if (isNaN(result)) {
-    postMessage('Please write two numbers');
+    self.postMessage('Please write two numbers');
   } else {
     const workerResult = 'Result: ' + result;
     console.log('Worker: Posting message back to main script');
-    postMessage(workerResult);
+    self.postMessage(workerResult);
   }
 }
